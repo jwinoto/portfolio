842d295afb1cda00d970abd16901cddbd8ddb8db
jwinoto <jwinoto39@gmail.com>
2026-01-12 21:00:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ hamburger.addEventListener('click', function() {
 
 nav_links.addEventListener('click', () => {
     nav_links.classList.toggle('show');
-})
+});
 
-};
+const frontEndButton = document.getElementById('frontend');
+const supportButton = document.getElementById('support');
+
+supportButton.addEventListener('click', function() {
+    document.querySelector('.it_support').style.display = 'grid';
+    document.querySelector('.projecten').style.display = 'none';
+});
+
+frontEndButton.addEventListener('click', function() {
+    document.querySelector('.it_support').style.display = 'none';
+    document.querySelector('.projecten').style.display = 'grid';
+});
+
+}
